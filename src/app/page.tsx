8c91f45b1ddf3df5cb531754c670cd34cd7be0c5
VJ solutions <vjysolutions@gmail.com>
2025-11-14 13:32:50 +0000

    can move Transaction History to below and Log a Transaction should be pr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,11 +94,11 @@ export default function Home() {
             </div>
           </section>
 
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
-             <div className="lg:col-span-1">
+          <div className="grid grid-cols-1 gap-8 items-start">
+             <div>
                 <TransactionForm onTransaction={handleTransaction} balances={balances} />
             </div>
-            <div className="lg:col-span-2">
+            <div>
                 <TransactionHistory transactions={transactions} />
             </div>
           </div>
